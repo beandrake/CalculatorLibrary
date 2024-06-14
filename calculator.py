3ca2e237c89a54b61d7f790f0811331361ae6145
Branden Bean Drake <30214543+beandrake@users.noreply.github.com>
2024-06-14 14:41:16 -0700

    Pushing, ready to try this Continuous Integration with circleci

---
+++ calculator.py
@@ -0,0 +1,14 @@
+###
+### Created to explore some Continuous Integration techniques with Python tools:
+###		Based on: https://realpython.com/python-continuous-integration/
+###
+
+"""
+Calculator library containing basic math operations.
+"""
+
+def add(first_term, second_term):
+	return first_term + second_term
+
+def subtract(first_term, second_term):
+	return first_term - second_term
