66caf11ec9080e41cd9a20a5d56c9988e5006d69
Branden Bean Drake <30214543+beandrake@users.noreply.github.com>
2024-06-25 11:27:58 -0700

    Fixed the error that was purposely introduced.

--- calculator.py
+++ calculator.py
@@ -11,7 +11,7 @@ def add(first_term, second_term):
 	return first_term + second_term
 
 def subtract(first_term, second_term):
-	return first_term - second_term + 1
+	return first_term - second_term
 
 def multiply(first_term, second_term):
 	return first_term * second_term
